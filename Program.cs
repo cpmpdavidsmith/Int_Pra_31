b7a2b53328cd3c35480fcb52c77717d0f8fa77b1
David Smith <cpmpdavidsmith@icloud.com>
2024-01-14 14:47:24 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList ar = new SortedList();
+
+            ar.Add(1, "One Ring To Rule Them All");
+            ar.Add(2, "Sam helped Frodo");
+            ar.Add(3, "Snow White");
+
+            Console.WriteLine("The snow whhite value is at inex " + ar.IndexOfValue("Snow White") + ". :)");
+            Console.Read();
+        }
+    }
+}
